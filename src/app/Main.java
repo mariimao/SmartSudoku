0a4a6f200d16b2160bc0698f6de4c637c5a1e282
mariimao <66910658+mariimao@users.noreply.github.com>
2023-10-30 14:07:52 -0400

    added toString to Board entity

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,5 +7,7 @@ public class Main {
     public static void main(String[] args) {
         EasyBoard tester = new EasyBoard();
         System.out.println(tester);
+
+        System.out.println("hello");
     }
 }
