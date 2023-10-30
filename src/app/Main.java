f1343d205e3ce92c5079f9a321137d1165e44e3d
CloudyKrypton <janetfu04@gmail.com>
2023-10-30 14:20:01 -0400

    undo moo

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,8 +8,7 @@ public class Main {
     public static void main(String[] args) {
         EasyBoard easyTester = new EasyBoard();
         System.out.println(easyTester);
-
-        moo
+        
         HardBoard hardTester = new HardBoard();
         System.out.println(hardTester);
     }
