f3de01de3f03a70642ef41f7363091064d70b56b
CloudyKrypton <janetfu04@gmail.com>
2023-10-30 14:05:08 -0400

    Generate hardboard implementation

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,11 +1,15 @@
 package app;
 
 import entity.EasyBoard;
+import entity.HardBoard;
 
 public class Main {
 
     public static void main(String[] args) {
-        EasyBoard tester = new EasyBoard();
-        System.out.println(tester);
+        EasyBoard easyTester = new EasyBoard();
+        System.out.println(easyTester);
+
+        HardBoard hardTester = new HardBoard();
+        System.out.println(hardTester);
     }
 }
