f00d46bb008b440cfa8e3f9c925fd29561cda200
tanjenn5 <jenn.tan@mail.utoronto.ca>
2023-11-10 15:13:31 -0500

    Added opening menu use case and updated start screen to change to menu view when button is pressed

--- src/interface_adapter/start/StartPresenter.java
+++ src/interface_adapter/start/StartPresenter.java
@@ -1,27 +1,36 @@
 package interface_adapter.start;
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.menu.MenuState;
+import interface_adapter.menu.MenuViewModel;
 import use_case.start.StartOutputBoundary;
 import use_case.start.StartOutputData;
 import view.StartView;
 
 import javax.swing.text.View;
+import java.awt.*;
 
 public class StartPresenter implements StartOutputBoundary {
 
     private final StartViewModel startViewModel;
+    private final MenuViewModel menuViewModel;
 
     private ViewManagerModel viewManagerModel;
 
-    public StartPresenter (StartViewModel startViewModel, ViewManagerModel viewManagerModel) {
+    public StartPresenter (StartViewModel startViewModel, MenuViewModel menuViewModel, ViewManagerModel viewManagerModel) {
         this.startViewModel = startViewModel;
         this.viewManagerModel = viewManagerModel;
+        this.menuViewModel = menuViewModel;
     }
 
     public void prepareSuccessView() {
-        StartState startState = startViewModel.getStartState();
-        this.startViewModel.setStartState(startState);
-        startViewModel.firePropertyChanged();
+
+        // changes view to menu view
+        MenuState menuState = menuViewModel.getMenuState();
+        this.menuViewModel.setMenuState(menuState);
+        menuViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveViewName(menuViewModel.getViewName());
     }
 
     public void prepareFailView(String error) {
