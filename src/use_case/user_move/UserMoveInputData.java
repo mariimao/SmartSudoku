75b9c39302cb9e1cc673726817ce6b7df150ba6e
annehuynh <anne.huynh@mail.utoronto.ca>
2023-11-21 00:23:19 -0500

    updated UserMoveInputData to add more relevant methods

--- src/use_case/user_move/UserMoveInputData.java
+++ src/use_case/user_move/UserMoveInputData.java
@@ -6,11 +6,33 @@ import entity.GameState;
 public class UserMoveInputData {
 
     final private GameState current_state;
+    final private int x;
+    final private int y;
+    final private int value;
 
-    public UserMoveInputData(GameState currentState) {
+    public UserMoveInputData(GameState currentState, int x, int y, int value) {
         current_state = currentState;
+        this.x = x;
+        this.y = y;
+        this.value = value;
+
 }
 
     Board getCurrent_board() {return current_state.getCurrBoard();}
-    int getLives() {return current_state.getLives();}
+
+    void loseLife() {current_state.loseLife();}
+
+    Board makeMove() {return current_state.makeMove(x, y, value);}
+
+    boolean gameOver() {return current_state.gameOver();}
+
+    int getDifficulty() {return current_state.getDifficulty();}
+
+    int getX() {return x;}
+    int getY() {return y;}
+    int getValue() {return value;}
+
+
+
     }
+
