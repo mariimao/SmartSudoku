2f926f53070331dd901d55d140eb2bf28b784ca4
annehuynh <anne.huynh@mail.utoronto.ca>
2023-11-21 00:25:06 -0500

    updated UserMoveInteractor to implement what happens when a user tries to make a move

--- src/use_case/user_move/UserMoveInteractor.java
+++ src/use_case/user_move/UserMoveInteractor.java
@@ -1,7 +1,53 @@
 package use_case.user_move;
 
-public class UserMoveInteractor implements PauseGameInputBoundary{
-    public void execute() {
+import entity.Board;
+import entity.GameState;
+import entity.User;
+import use_case.login.LoginInputData;
+import use_case.login.LoginOutputBoundary;
+import use_case.login.LoginOutputData;
 
+public class UserMoveInteractor implements UserMoveInputBoundary {
+
+    final UserMoveDataAccessInterface userMoveDataAccessInterface;
+    final UserMoveOutputBoundary gamePresenter;
+
+    public UserMoveInteractor(UserMoveDataAccessInterface userMoveDataAccessInterface, UserMoveOutputBoundary gamePresenter) {
+        this.userMoveDataAccessInterface = userMoveDataAccessInterface;
+        this.gamePresenter = gamePresenter;
+    }
+
+    public void execute(UserMoveInputData userMoveInputData) {
+        Board current_board = userMoveInputData.getCurrent_board();
+        int difficulty = userMoveInputData.getDifficulty();
+        int x = userMoveInputData.getX();
+        int y = userMoveInputData.getY();
+        int value = userMoveInputData.getValue();
+
+        if (userMoveInputData.gameOver()) {
+            gamePresenter.prepareEndView(userMoveOutputData);
+        } else {
+            if (difficulty == 1) {
+                int[][] easyPossibleValues = current_board.generatePossibleValues();
+                if (current_board.valueNotAvailable(easyPossibleValues, value, x, y)) {
+                    userMoveInputData.loseLife();
+                    gamePresenter.prepareFailView("Incorrect input. Try again.");
+                } else {
+                    Board new_board = userMoveInputData.makeMove();
+                    gamePresenter.prepareSuccessView(userMoveOutputData);
+                }
+            } else {
+                int[][] hardPossibleValues = current_board.generatePossibleValues();
+                if (current_board.valueNotAvailable(hardPossibleValues, value, x, y)) {
+                    userMoveInputData.loseLife();
+                    gamePresenter.prepareFailView("Incorrect input. Try again.");
+                } else if (userMoveInputData.gameOver()) {
+                    gamePresenter.prepareEndView(userMoveOutputData);
+                } else {
+                    userMoveInputData.makeMove();
+                    gamePresenter.prepareSuccessView(userMoveOutputData);
+                }
+            }
+        }
     }
 }
