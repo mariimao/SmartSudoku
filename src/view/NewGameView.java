98959d29b7dfa9c65754e9ae1cde2f776e9acf99
Akunna Megwa <akunna.megwa@gmail.com>
2023-11-24 13:35:40 -0500

    changed NewGameView to use the colours and look better

--- src/view/NewGameView.java
+++ src/view/NewGameView.java
@@ -6,6 +6,9 @@ import interface_adapter.new_game.NewGameState;
 import interface_adapter.new_game.NewGameViewModel;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -18,15 +21,33 @@ public class NewGameView extends JPanel implements ActionListener, PropertyChang
     // TODO: the viewModel and controller for the board view use case need to be added
     private final JButton createEasyGame;
     private final JButton createHardGame;
+    private final Color blue = new Color(97, 150, 242);
+    private final Color darkblue = new Color(50, 78, 156);
+    private final Color white = new Color(255, 255, 255);
+    private final Color black = new Color(0, 0, 0);
 
     public NewGameView(NewGameViewModel newGameViewModel, NewGameController newGameController) {
         this.newGameViewModel = newGameViewModel;
         this.newGameController = newGameController;
+
+        newGameViewModel.addPropertyChangeListener(this);
+        this.setBackground(white);
+
+        // creating the title
         JLabel title = new JLabel("Choose Game Difficulty");
+        title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        title.setFont(new Font("Helvetica", Font.BOLD, 90));
+        title.setForeground(darkblue);
+        title.setBorder(new CompoundBorder(title.getBorder(), new EmptyBorder(10,40,10,40)));
+        this.add(title);
+
+        //  creating the buttons
         JPanel buttons = new JPanel();
-        createEasyGame = new JButton("Easy Game");
+        createEasyGame = new CustomButton("Easy Game", darkblue, white);
+        createEasyGame.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         buttons.add(createEasyGame);
-        createHardGame = new JButton("Hard Game");
+        createHardGame = new CustomButton("Hard Game", darkblue, white);
+        createHardGame.setAlignmentX(JLabel.CENTER_ALIGNMENT);
         buttons.add(createHardGame);
 
         createEasyGame.addActionListener(
