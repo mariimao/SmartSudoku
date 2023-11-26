4b809c8b5dd2ffdb9d64b9e6114029aef98f364a
annehuynh <anne.huynh@mail.utoronto.ca>
2023-11-25 20:11:28 -0500

    implemented end game view model

---
+++ src/interface_adapter/end_game/EndGameViewModel.java
@@ -0,0 +1,35 @@
+package interface_adapter.end_game;
+
+import interface_adapter.ViewModel;
+import interface_adapter.easy_game.EasyGameState;
+import interface_adapter.new_game.NewGameState;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class EndGameViewModel extends ViewModel {
+    public static final String MENU_BUTTON_LABEL = "Menu";
+    public static final String LEADERBOARD_BUTTON_LABEL = "Leaderboard";
+
+    private EndGameState state = new EndGameState();
+
+    public EndGameViewModel() {super("end game view");}
+
+    public void setState(EndGameState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    @Override
+    public void firePropertyChanged() {support.firePropertyChange("state", null, this.state);}
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {support.addPropertyChangeListener(listener);}
+
+    public EndGameState getState() {
+        return this.state;
+    }
+
+}
+
