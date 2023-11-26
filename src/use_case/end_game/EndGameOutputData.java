33ef6f2237cd75a8ffce58ee48d5372d573a7f9e
annehuynh <anne.huynh@mail.utoronto.ca>
2023-11-25 20:10:49 -0500

    implemented end game output data

---
+++ src/use_case/end_game/EndGameOutputData.java
@@ -0,0 +1,17 @@
+package use_case.end_game;
+
+import entity.board.GameState;
+import entity.user.User;
+
+public class EndGameOutputData {
+    private final User user;
+    private final int score;
+
+    public EndGameOutputData(User user, int score) {
+
+        this.user = user;
+        this.score = score;
+    }
+    public int getScore() {return score;}
+    public User getUser() {return user;}
+}
