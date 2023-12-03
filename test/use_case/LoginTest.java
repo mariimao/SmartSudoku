81dc414475c2422967227b2bb6a152f4cfac2c01
tanjenn5 <jenn.tan@mail.utoronto.ca>
2023-12-02 23:11:09 -0500

    login interactor tests added, but running into problem where tests are being saved to mongo database as well

--- test/use_case/LoginTest.java
+++ test/use_case/LoginTest.java
@@ -1,9 +1,13 @@
+package use_case;
+
 import data_access.UserDAO;
 import entity.user.CommonUserFactory;
 import entity.user.User;
 import entity.user.UserFactory;
+import org.junit.Before;
 import org.junit.Test;
-import use_case.signup.*;
+import org.junit.jupiter.api.BeforeEach;
+import use_case.login.*;
 
 import java.time.LocalTime;
 import java.util.HashMap;
@@ -11,76 +15,14 @@ import java.util.Map;
 
 import static org.junit.Assert.*;
 
-public class SigninTest {
-
-    SignupUserDataAccessInterface userDataBase;
-
-    @Test
-    public void testSuccessCreation() {
-        SignupInputData inputData = new SignupInputData("name", "pass", "pass");
-        SignupUserDataAccessInterface userDataAccessObject;
-        try {
-            userDataAccessObject = new UserDAO("mongodb+srv://smartsudoku:smartsudoku@cluster0.hbx3f3f.mongodb.net/\n\n",
-                    "smartsudoku", "user", new CommonUserFactory());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        userDataBase = userDataAccessObject;
-
-        SignupOutputBoundary creationPresenter = new SignupOutputBoundary() {
-            @Override
-            public void prepareSuccessView(SignupOutputData signupOutputData) {
-                assertEquals("name", signupOutputData.getUsername());
-                assertTrue(userDataBase.existsByName("name"));
-            }
-
-            @Override
-            public void prepareFailView(String error) {
-                fail("Use case failed but is unexpected.");
-            }
-        };
-
-        Map<LocalTime, Integer> scores = new HashMap<>();
-        scores.put(LocalTime.now(), 100);
-        SignupInputBoundary interactor = new SignupInteractor(userDataBase, creationPresenter, new CommonUserFactory(), scores);
-        interactor.execute(inputData);
-    }
+public class LoginTest {
+    LoginUserDataAccessInterface userDataBase;
 
     @Test
-    public void testIncorrectTest() {
-        SignupInputData inputData = new SignupInputData("name", "pass", "wrong");
-        SignupUserDataAccessInterface userDataAccessObject;
-        try {
-            userDataAccessObject = new UserDAO("mongodb+srv://smartsudoku:smartsudoku@cluster0.hbx3f3f.mongodb.net/\n\n",
-                    "smartsudoku", "user", new CommonUserFactory());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        userDataBase = userDataAccessObject;
+    public void testSuccessLogin() {
 
-        SignupOutputBoundary creationPresenter = new SignupOutputBoundary() {
-            @Override
-            public void prepareSuccessView(SignupOutputData signupOutputData) {
-                fail("Use case succeeded but is unexpected.");
-            }
-
-            @Override
-            public void prepareFailView(String error) {
-                assertEquals("Passwords do not match. Try again.", error);
-            }
-        };
-
-        Map<LocalTime, Integer> scores = new HashMap<>();
-        scores.put(LocalTime.now(), 100);
-        SignupInputBoundary interactor = new SignupInteractor(userDataBase, creationPresenter, new CommonUserFactory(), scores);
-        interactor.execute(inputData);
-
-    }
-
-    @Test
-    public void testExistingUser() {
-        SignupInputData inputData = new SignupInputData("username", "pass", "pass");
-        SignupUserDataAccessInterface userDataAccessObject;
+        LoginInputData inputData = new LoginInputData("name", "pass");
+        LoginUserDataAccessInterface userDataAccessObject;
         try {
             userDataAccessObject = new UserDAO("mongodb+srv://smartsudoku:smartsudoku@cluster0.hbx3f3f.mongodb.net/\n\n",
                     "smartsudoku", "user", new CommonUserFactory());
@@ -91,24 +33,96 @@ public class SigninTest {
 
         UserFactory userFactory = new CommonUserFactory();
         Map<LocalTime, Integer> scores_test = new HashMap<>();
-        User user = userFactory.create("username", "pass1", scores_test);
+        User user = userFactory.create("name", "pass", scores_test);
         userDataBase.addUser(user);
 
-        SignupOutputBoundary creationPresenter = new SignupOutputBoundary() {
+        LoginOutputBoundary loginPresenter = new LoginOutputBoundary() {
             @Override
-            public void prepareSuccessView(SignupOutputData signupOutputData) {
-                fail("Use case success but is unexpected");
+            public void prepareSuccessView(LoginOutputData loginOutputData) {
+                assertTrue(userDataBase.existsByName("name"));
+                assertEquals("name", loginOutputData.getName());
             }
 
             @Override
             public void prepareFailView(String error) {
-                assertEquals("Username already exists. Please pick a different username.", error);
+                fail("Use case failed but is unexpected.");
             }
         };
 
         Map<LocalTime, Integer> scores = new HashMap<>();
         scores.put(LocalTime.now(), 100);
-        SignupInputBoundary interactor = new SignupInteractor(userDataBase, creationPresenter, new CommonUserFactory(), scores);
+        LoginInputBoundary interactor = new LoginInteractor(userDataAccessObject, loginPresenter);
         interactor.execute(inputData);
+    }
+
+    @Test
+    public void testUserDNE() {
+        LoginInputData inputData = new LoginInputData("1234567890", "pass");
+        LoginUserDataAccessInterface userDataAccessObject;
+        try {
+            userDataAccessObject = new UserDAO("mongodb+srv://smartsudoku:smartsudoku@cluster0.hbx3f3f.mongodb.net/\n\n",
+                    "smartsudoku", "user", new CommonUserFactory());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        userDataBase = userDataAccessObject;
+
+        LoginOutputBoundary loginPresenter = new LoginOutputBoundary() {
+            @Override
+            public void prepareSuccessView(LoginOutputData loginOutputData) {
+                fail("Use case success but is unexpected");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertFalse(userDataBase.existsByName("1234567890"));
+                assertEquals("1234567890: Account does not exist.", error);
+            }
+        };
+
+        Map<LocalTime, Integer> scores = new HashMap<>();
+        scores.put(LocalTime.now(), 100);
+        LoginInputBoundary interactor = new LoginInteractor(userDataBase, loginPresenter);
+        interactor.execute(inputData);
+
+    }
+
+    @Test
+    public void testPasswordIncorrect() {
+
+        LoginInputData inputData = new LoginInputData("name", "wrong");
+        LoginUserDataAccessInterface userDataAccessObject;
+        try {
+            userDataAccessObject = new UserDAO("mongodb+srv://smartsudoku:smartsudoku@cluster0.hbx3f3f.mongodb.net/\n\n",
+                    "smartsudoku", "user", new CommonUserFactory());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        userDataBase = userDataAccessObject;
+
+        UserFactory userFactory = new CommonUserFactory();
+        Map<LocalTime, Integer> scores_test = new HashMap<>();
+        User user = userFactory.create("name", "pass", scores_test);
+        userDataBase.addUser(user);
+
+        LoginOutputBoundary loginPresenter = new LoginOutputBoundary() {
+            @Override
+            public void prepareSuccessView(LoginOutputData loginOutputData) {
+                fail("Use case success but is unexpected");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                // user name exists but username is wrong
+                assertTrue(userDataBase.existsByName("name"));
+                assertEquals("Incorrect password. Try again.", error);
+            }
+        };
+
+        Map<LocalTime, Integer> scores = new HashMap<>();
+        scores.put(LocalTime.now(), 100);
+        LoginInputBoundary interactor = new LoginInteractor(userDataBase, loginPresenter);
+        interactor.execute(inputData);
+
     }
 }
