380245d242a387e234c3c844b68e7918878d593e
tanjenn5 <jenn.tan@mail.utoronto.ca>
2023-12-04 03:22:32 -0500

    Fixed Errors in previous files  + added javadoc for start interface adapters

--- src/interface_adapter/start/StartController.java
+++ src/interface_adapter/start/StartController.java
@@ -4,14 +4,25 @@ import use_case.start.StartInputBoundary;
 import use_case.start.StartInputData;
 import use_case.start.StartInteractor;
 
+/**
+ * The class for StartController. Acts as controller to send information to StartInteractor.
+ */
 public class StartController {
 
     final StartInputBoundary startUseCaseInteractor;
 
+    /**
+     * Constructor of Start Controller
+     * @param startUseCaseInteractor the interactor that makes decisions with input data
+     */
     public StartController(StartInputBoundary startUseCaseInteractor) {
         this.startUseCaseInteractor = startUseCaseInteractor;
     }
 
+    /**
+     * Executes the use case's interactor to perform action
+     * @param interaction string that identifies what type of button was pressed
+     */
     public void execute (String interaction) {
         StartInputData startInputData = new StartInputData(interaction);
         startUseCaseInteractor.execute(startInputData);
--- src/interface_adapter/start/StartState.java
+++ src/interface_adapter/start/StartState.java
@@ -1,4 +1,7 @@
 package interface_adapter.start;
 
+/**
+ * Class of the Start State. Since this use case is just switching views, this is not very important.
+ */
 public class StartState {
 }
